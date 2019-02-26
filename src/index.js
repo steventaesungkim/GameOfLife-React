908ff3803ca9ccf92db7b87c762d0765de6cd032
Steven Taesung Kim <steventaesungkim@gmail.com>
2019-02-25 21:20:27 -0500

    setup for Main class componenet

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Main from './Main';
 
-ReactDOM.render(<p> hello game of life</p>, document.getElementById('root'));
+ReactDOM.render(<Main/>, document.getElementById('root'));
