38fbff101b48f2afdc398b94bb343033d3e8c9a7
Steven Taesung Kim <steventaesungkim@gmail.com>
2019-02-25 21:20:50 -0500

    setup for Main class componenet

---
+++ src/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <h1>The Game of Life</h1>
+                {/* <Grid /> */}
+                {/* <h2>Generations: {this.state.generation}</h2> */}
+            </div>
+        );
+    }
+}
+
+export default Main;
